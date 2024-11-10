880bfc69c460baaa16acb32e9bec4dc811189a78
povidllo <kuzminovm78@gmail.com>
2024-11-10 15:47:55 +0700

    damped harmonic oscillator

--- neural_network.py
+++ neural_network.py
@@ -33,11 +33,6 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #     y = eq(x) +  0.8 *np.random.rand()
 #     return x, y
 
-def fourier_feature(x, in_dim, map):
-        freqs = nn.Parameter(torch.randn(map, in_dim))  # Размерность частот
-        scale = nn.Parameter(torch.randn(map))  # Масштабирование
-        sinusoid = torch.cat([torch.sin(x @ freqs.T + scale), torch.cos(x @ freqs.T + scale)], dim=-1)
-        return sinusoid
 
 
 class simpleModel(nn.Module):
@@ -79,8 +74,8 @@ class simpleModel(nn.Module):
     def forward(self, x):
         if(self.fourie):
             # encoding = rff.layers.PositionalEncoding(sigma=1.0, m=self.mapped_fourie)
-            # encoding = rff.layers.BasicEncoding()
-            encoding = rff.layers.GaussianEncoding(sigma=5.0, input_size=self.input_size, encoded_size=self.mapped_fourie)         
+            encoding = rff.layers.BasicEncoding()
+            # encoding = rff.layers.GaussianEncoding(sigma=5.0, input_size=self.input_size, encoded_size=self.mapped_fourie)         
             xf = encoding(x)
             return self.layers_stack(xf)
             # xf = fourier_feature(x, self.input_size, self.mapped_fourie)
@@ -97,6 +92,7 @@ class simpleModel(nn.Module):
         pbar = tqdm(range(steps), desc='Training Progress')
         writer = SummaryWriter()
         optimizer = torch.optim.LBFGS(self.parameters(), self.lr)
+        # optimizer = torch.optim.Adam(self.parameters, self.lr)
         for step in pbar:
             def closure():
                 optimizer.zero_grad()
