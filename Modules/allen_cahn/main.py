25acf139abcfb3f2da60d25140c2b78cb69d5da4
povidllo <kuzminovm78@gmail.com>
2025-02-12 17:32:22 +0700

    updated allen_cahn

--- Modules/allen_cahn/main.py
+++ Modules/allen_cahn/main.py
@@ -15,30 +15,30 @@ from Modules.allen_cahn.loss_calc import loss_calculator
 from Modules.allen_cahn.calculate_l2 import calculate_l2_error
 from Modules.allen_cahn.vizualizer import vizualize
 from Modules.allen_cahn.test_data_generator import generator as test_data_generator
-import cfg_pinn_init as cfg_pinn_init
-import cfg_opt_Adam_torch as cfg_opt_Adam_torch
-import cfg_train_torch as cfg_train_torch
+# import cfg_pinn_init as cfg_pinn_init
+import cfg_main as cfg_main
+# import cfg_train_torch as cfg_train_torch
 
-torch.manual_seed(44)
-np.random.seed(44)
-torch.cuda.manual_seed(44)
+torch.manual_seed(123)
+# np.random.seed(44)
+# torch.cuda.manual_seed(44)
 
-model = pinn(cfg_pinn_init.get_config())
+model = pinn(cfg_main.get_config())
 # Вывод весов при инициализации
 # for name, param in model.named_parameters():
 #     print(f"\nLayer: {name}")
 #     print(f"Shape: {param.shape}")
 #     print(f"Values:\n{param.data}")
+# exit()
 
-optimizer = create_optim(model, cfg_opt_Adam_torch.get_config()) 
+optimizer = create_optim(model, cfg_main.get_config()) 
 
-trainer = Train_torch(cfg_train_torch.get_config(),
-                      model, 
-                      optimizer, 
-                      data_generator, 
-                      loss_calculator,
-                      test_data_generator,
-                      calculate_l2_error,   
-                      vizualize)
-# trainer.train()
-trainer.printEval()
+trainer = Train_torch(cfg_main.get_config(),
+                    model, 
+                    optimizer, 
+                    data_generator, 
+                    loss_calculator,
+                    test_data_generator,
+                    calculate_l2_error,
+                    vizualize)
+trainer.train()
