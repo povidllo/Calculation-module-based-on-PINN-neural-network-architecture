149583b629d8dd4a7433652d6cd70c558d47d02d
Mikle Baev <m.baev@g.nsu.ru>
2025-02-12 11:38:01 +0700

    New branch

--- raw_database.py
+++ raw_database.py
@@ -1,4 +1,4 @@
-from typing import Optional, List, Callable
+from typing import Optional, List
 from beanie import Document
 
 class HyperParam(Document):
@@ -15,27 +15,6 @@ class HyperParam(Document):
   fourier_dim: Optional[int]
   fourier_scale: Optional[int]
 
-class DataSet(Document):
-    power_time_vector: int
-    power_dom_vector: int
-    param: List[int]
-    data_generator: Callable
-    loss_calc: Callable
-
-class NeuralNetwork(Document):
-  hyperparam_id: int
-  weight_id: int
-
-  init: Callable
-  inference: Callable
 
 class Weight(Document):
-  weights: List[int]
-
-class Optimizer(Document):
-  data_set_id: int
-  neural_network_id: int
-  train: Callable
-  finetune: Callable
-  save_weights: Callable
-  show_res: Callable
+  file_path: str
