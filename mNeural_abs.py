0c0ab747ac4e6db901bdab882d4ac76c7b4b4682
qandy <sss>
2025-02-13 20:14:39 +0700

    small refactoring

---
+++ mNeural_abs.py
@@ -0,0 +1,40 @@
+from mongo_schemas import *
+import abc
+
+
+class abs_neural_net(abc.ABC):
+    neural_model : mNeuralNet_mongo = None
+
+
+    async def createModel(self, params : mHyperParams):
+        self.neural_model = mNeuralNet_mongo(hyper_param=mHyperParams_mongo(**params.model_dump()))
+        self.neural_model.records = []
+        await self.set_dataset()
+
+        await mNeuralNet_mongo.m_insert(self.neural_model)
+
+    async def update_neural_model(self):
+        await mNeuralNet_mongo.m_save(self.neural_model)
+
+    async def update_dataset_for_nn(self, new_dataset : mDataSet_mongo):
+        await self.neural_model.data_set[0].delete()
+        self.neural_model.data_set = [new_dataset]
+
+
+    @abc.abstractmethod
+    async def construct_model(self, params : mHyperParams, in_device): pass
+
+    @abc.abstractmethod
+    def set_optimizer(self, opti : mOptimizer = None): pass
+
+    @abc.abstractmethod
+    async def set_dataset(self, dataset : mDataSet = None): pass
+
+    @abc.abstractmethod
+    def load_model(self, in_model : mNeuralNet): pass
+
+    @abc.abstractmethod
+    def train(self): pass
+
+    @abc.abstractmethod
+    async def calc(self): pass
--- neural_page_bff.py
+++ neural_page_bff.py
@@ -27,6 +27,11 @@ import motor.motor_asyncio
 from motor.motor_asyncio import AsyncIOMotorClient
 import asyncio
 
+router = APIRouter()
+
+
+
+
 async def create_neural_model(model_type : Optional[str] = None, inNu : Optional[int] = None, desc : Optional[str] = 'hello world'):
     if (model_type is not None):
         params = mHyperParams(mymodel_type=model_type, mymodel_desc=desc)
@@ -36,6 +41,8 @@ async def create_neural_model(model_type : Optional[str] = None, inNu : Optional
             await neural_net_manager.set_dataset(mdataset)
 
     return {"resp" : "OK"}
+
+
 
 async def train_neural_net():
     res = await neural_net_manager.train_model()
@@ -57,7 +64,7 @@ async def run_neural_net():
 
     return HTMLResponse(mTemplate.mtemplate('<script></script>', table_templ))
 
-router = APIRouter()
+
 
 def main(loop):
 
@@ -91,232 +98,3 @@ if __name__ == '__main__':
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     main(loop)
-
-
-# import torch
-# import torch.nn as nn
-#
-# import torch.optim as optim
-# from torch.utils.data import Dataset, DataLoader
-#
-# from tqdm import tqdm
-# from torch.utils.tensorboard import SummaryWriter
-# # from google.colab import files as filescolab
-#
-#
-# from torchviz import make_dot
-#
-# from torchvision import models
-# from torchsummary import summary
-# import hiddenlayer as hl
-
-
-# client = AsyncIOMotorClient("mongodb://"+database_ulr+":27017")
-#
-# async def init():
-#     await init_beanie(database=client[database_name], document_models=[mongo_Estimate, mongo_Record])
-#
-# torch.manual_seed(20)
-#
-# api_v1 = jsonrpc.Entrypoint('/api/v1/jsonrpc')
-#
-# class simpleModel(nn.Module):
-#   def __init__(self,
-#                hidden_size=20):
-#     super().__init__()
-#     self.layers_stack = nn.Sequential(
-#         nn.Linear(1, hidden_size),
-#         nn.Tanh(),
-#         nn.Linear(hidden_size, hidden_size), #1
-#         nn.Tanh(),
-#         nn.Linear(hidden_size, hidden_size), #2
-#         nn.Tanh(),
-#         nn.Linear(hidden_size, hidden_size), #3
-#         nn.Tanh(),
-#         nn.Linear(hidden_size, hidden_size), #4
-#         nn.Tanh(),
-#         nn.Linear(hidden_size, 1),
-#         nn.Tanh(),
-#     )
-#
-#
-#   def forward(self, x):
-#     return self.layers_stack(x)
-#
-# isTrained = False
-# isTraining = False
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#
-# x0_true=torch.tensor([1], dtype=float).float().to(device)
-# dx0dt_true = torch.tensor([0], dtype=float).float().to(device)
-
-# model = simpleModel().to(device)
-#
-# def pde(out, t, nu=3):
-#     omega = 2 * torch.pi * nu
-#     dxdt = torch.autograd.grad(out, t, torch.ones_like(t), create_graph=True, \
-#                             retain_graph=True)[0]
-#     d2xdt2 = torch.autograd.grad(dxdt, t, torch.ones_like(t), create_graph=True, \
-#                             retain_graph=True)[0]
-#     f = d2xdt2 + (omega ** 2) * out
-#     return f
-#
-#
-# def pdeBC(t):
-#     metric_data = nn.MSELoss()
-#
-#     out = model(t).to(device)
-#     f1 = pde(out, t)
-#
-#     inlet_mask = (t[:, 0] == 0)
-#     t0 = t[inlet_mask]
-#     x0 = model(t0).to(device)
-#     dx0dt = torch.autograd.grad(x0, t0, torch.ones_like(t0), create_graph=True, \
-#                         retain_graph=True)[0]
-#
-#     loss_bc = metric_data(x0, x0_true) + \
-#                 metric_data(dx0dt, dx0dt_true.to(device))
-#     loss_pde = metric_data(f1, torch.zeros_like(f1))
-#
-#     loss = 1e3*loss_bc + loss_pde
-#
-#     return loss
-#
-#
-# def train(in_steps):
-#     global isTrained
-#     global isTraining
-#     isTraining = True
-#     steps = in_steps
-#     pbar = tqdm(range(steps), desc='Training Progress')
-#     optimizer = torch.optim.LBFGS(model.parameters(), lr=0.1)
-#     t = (torch.linspace(0, 1, 100).unsqueeze(1)).to(device)
-#     t.requires_grad = True
-#
-#     for step in pbar:
-#         def closure():
-#             optimizer.zero_grad()
-#             loss = pdeBC(t)
-#             loss.backward()
-#             return loss
-#
-#         optimizer.step(closure)
-#         if step % 2 == 0:
-#             current_loss = closure().item()
-#             pbar.set_description("Step: %d | Loss: %.6f" %
-#                                  (step, current_loss))
-#     isTrained = True
-#     isTraining = False
-#
-#
-#
-#
-# async def predict():
-#     nu = 3
-#     omega = 2 * torch.pi * nu
-#
-#
-#     t = torch.linspace(0, 1, 100).unsqueeze(-1).unsqueeze(0).to(device)
-#     t.requires_grad = True
-#     x_pred = model(t.float())
-#     # print('x_pred ', type(x_pred.tolist()), x_pred.tolist())
-#
-#
-#
-#     # est = mongo_Estimate(records=[mongo_Record(record=x_pred.tolist())])
-#     # await mongo_Estimate.m_insert(est)
-#     # print('est ', type(est), est)
-#
-#     x_true = x0_true * torch.cos(omega*t)
-#
-#     fs = 13
-#     plt.scatter(t[0].cpu().detach().numpy(), x_pred[0].cpu().detach().numpy(), label='pred',
-#                 marker='o',
-#                 alpha=.7,
-#                 s=50)
-#     plt.plot(t[0].cpu().detach().numpy(), x_true[0].cpu().detach().numpy(),
-#              color='blue',
-#              label='analytical')
-#     plt.xlabel('t', fontsize=fs)
-#     plt.ylabel('x(t)', fontsize=fs)
-#     plt.xticks(fontsize=fs)
-#     plt.yticks(fontsize=fs)
-#     plt.legend()
-#     plt.title('x(t)')
-#     # plt.savefig('x.png')
-#     # plt.show()
-#
-#     my_stringIObytes = io.BytesIO()
-#     plt.savefig(my_stringIObytes, format='jpg')
-#     my_stringIObytes.seek(0)
-#     my_base64_jpgData = base64.b64encode(my_stringIObytes.read()).decode()
-#
-#
-#     plt.clf()
-#
-#     # transforms = [hl.transforms.Prune('Constant')]
-#     # graph = hl.build_graph(model, batch.text, transforms=transforms)
-#     # graph.theme = hl.graph.THEMES['blue'].copy()
-#     # graph.save('rnn_hiddenlayer', format='png')
-#
-#     # vizNN = make_dot(x_pred.mean(), params=dict(model.named_parameters()))
-#     # print('vizNN', vizNN)
-#     # print('model', model)
-#     # my_base64_jpgData = np.concatenate((my_base64_jpgData, vizNN), axis=1)
-#
-#
-#
-#
-#     return my_base64_jpgData
-#
-#
-#
-# async def pinn() :
-#     global isTrained
-#
-#     base64_encoded_image = b''
-#
-#     loader = jinja2.FileSystemLoader("./templates")
-#     env = jinja2.Environment(loader=loader,autoescape = False)
-#     table_templ = env.get_template("index.html")
-#
-#     if isTrained:
-#         base64_encoded_image = await predict()
-#
-#     table_templ = table_templ.render(myImage=base64_encoded_image)
-#
-#     return HTMLResponse(mTemplate.mtemplate('<script></script>', table_templ))
-#
-#
-# async def train_pinn(background_tasks: BackgroundTasks, N:Optional[int] = None):
-#     global isTrained
-#     global isTraining
-#     steps = 10
-#     if (N is not None) and (not isTraining):
-#         isTrained = False
-#         steps = N
-#
-#
-#     # all_est = await mongo_Estimate.find_entries_all()
-#     # print('all_est', all_est[0].id)
-#     # ret_all_est = [str(el.id) for el in all_est]
-#     # print('ret_all_est', ret_all_est)
-#     if (not isTrained) and (not isTraining):
-#         background_tasks.add_task(train, steps)
-#     # return {"ok" : isTrained , 'training' : isTraining, 'ests': ret_all_est}
-#     return {"ok" : isTrained , 'training' : isTraining}
-# if __name__ == '__main__':
-#
-#     @asynccontextmanager
-#     async def lifespan(app: jsonrpc.API):
-#         await init()
-#         yield
-#
-#
-#     app = jsonrpc.API(lifespan=lifespan)
-#     app.bind_entrypoint(api_v1)
-#     app.add_api_route("/pinn", pinn, methods=["GET"])
-#     app.add_api_route("/train", train_pinn, methods=["GET"])
-#
-#     import uvicorn
-#     uvicorn.run(app, host=cur_host, port=8010)
