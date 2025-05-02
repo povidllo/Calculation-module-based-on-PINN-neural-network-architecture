6ac7c69d557581f92ae39b727ac00d3636be39dd
YogNare <mysticnare@gmail.com>
2025-05-02 21:16:54 +0700

    dead inside

--- Modules/optim_Adam_torch.py
+++
@@ -1,7 +0,0 @@
-# class Opt_Adam_torch:
-#     def __init__(self,
-#                  model,
-#                  cfg):
-import torch
-def create_optim(model, cfg):
-    return torch.optim.Adam(model.parameters(), betas=cfg.betas, lr=cfg.lr)
